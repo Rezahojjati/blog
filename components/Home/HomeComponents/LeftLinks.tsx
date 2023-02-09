b3815b768316465f4e4e61a9c1bb24229269e638
Reza Hojjati <rezahojjati@Rezas-MacBook-Air.local>
2023-02-09 12:36:46 -0500

    search bar

--- components/Home/HomeComponents/LeftLinks.tsx
+++ components/Home/HomeComponents/LeftLinks.tsx
@@ -6,13 +6,14 @@ export default function LeftLinks() {
     <div className={styles.linkDiv}>
       <h4>1-2-1 LOVE TAPS</h4>
       <p>
-        Where we share one central theme, two or more questions, and one habit
-        to stimulate your curiosity, self-exploration and interoception — all in
-        the interest of supporting you in being better and shining more
-        brightly. To be better requires empowerment to upgrade and reboot
-        aspects of your personal operating system. Upgrades that allow for the
-        more complete expression of your potential, personal transformations and
-        the realization of peak experiences, more consistently.
+        Where we share one central theme, two or more questions, and at least
+        one habit to stimulate your curiosity, self-exploration and
+        interoception — all in the interest of supporting you in being better
+        and shining more brightly. To be better requires empowerment to upgrade
+        and reboot aspects of your personal operating system. Upgrades that
+        allow for the more complete expression of your potential, personal
+        transformations and the realization of peak experiences, more
+        consistently.
       </p>
     </div>
   );
